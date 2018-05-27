c40327b0f5ced24fce46ca3ee1da698fc9af898e
Osvaldo Gusmão <osvaldogusmao@yahoo.com.br>
2018-05-27 02:18:57 -0300

    Implementando o metodo remover

--- src/com/osvaldogusmao/estruturas/Vetor.java
+++ src/com/osvaldogusmao/estruturas/Vetor.java
@@ -4,32 +4,62 @@ public class Vetor {
 
 	private String[] elementos;
 	private int ultimaPosicao;
+	private int capacidade;
 
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
 		this.ultimaPosicao = 0;
+		this.capacidade = capacidade;
 	}
 
 	public void adiciona(String elemento) throws Exception {
-		if (this.ultimaPosicao <= this.elementos.length) {
+		if (this.ultimaPosicao <= this.elementos.length && this.ultimaPosicao < this.capacidade) {
 			this.elementos[this.ultimaPosicao] = elemento;
 			this.ultimaPosicao++;
-		} else {
+		}else if(this.ultimaPosicao == this.capacidade) {
+			this.elementos[this.retornaPosicaoVazia()] = elemento;
+		}else {
 			throw new Exception();
 		}
 	}
 
+	public void removeElemento(int posicao) {
+		this.elementos[posicao] = null;
+	}
+	
 	public int ultimaPosicao() {
-		return ultimaPosicao;
+		return this.ultimaPosicao;
+	}
+	
+	private int retornaPosicaoVazia() {
+		int pos = 0;
+		for (int i = 0; i < elementos.length; i++) {
+			if(elementos[i] == null) {
+				pos = i;
+			}
+		}
+		return pos;
 	}
 	
 	@Override
 	public String toString() {
 		
-		for (int i = 0; i < elementos.length; i++) {
-			
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		
+		for (int i = 0; i < this.ultimaPosicao-1; i++) {
+			if(elementos[i] != null) {
+				s.append(elementos[i].toString());
+				s.append(",");	
+			}
 		}
 		
-		return "Este elemento possui: " + this.elementos.length;
+		if (this.elementos.length >= this.ultimaPosicao) {
+			s.append(elementos[this.ultimaPosicao-1].toString());
+		}
+		
+		s.append("]");
+		
+		return s.toString();
 	}
 }
--- src/com/osvaldogusmao/estruturas/VetorTeste.java
+++ src/com/osvaldogusmao/estruturas/VetorTeste.java
@@ -2,15 +2,23 @@ package com.osvaldogusmao.estruturas;
 
 public class VetorTeste {
 	public static void main(String[] args) {
-		
-		Vetor vetor = new Vetor(2);
+
+		Vetor vetor = new Vetor(5);
 		try {
 			vetor.adiciona("José");
 			vetor.adiciona("Osvaldo");
-			vetor.adiciona("Elemento que vai dar erro");
+			vetor.adiciona("3 Elemento");
+			vetor.adiciona("Elaine");
+			vetor.removeElemento(2);
+			vetor.adiciona("3 Elemento Novo");
+			vetor.adiciona("4 Elemento");
+			vetor.removeElemento(0);
+			vetor.adiciona("Novo teste");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
+		System.out.println(vetor.toString());
+
 	}
 }
